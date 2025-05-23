de7a04cbecbe5ef4e9d7981038f49a6f2ae0d4d1
estelle-gf <estellegfont@gmail.com>
2025-05-23 23:58:58 +0100

    updated search functionality using API

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function changeWeather(response) {
-  console.log(response.data);
+  console.log(response);
+  let temperature = response.data.temperature.current;
+  let roundedTemperature = Math.round(temperature);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = roundedTemperature;
 }
+
 function searchCity(city) {
   let apiKey = "2a99380b94355b9foa25076te09bd049";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(changeWeather);
 }
 
@@ -11,9 +18,11 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
   searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
