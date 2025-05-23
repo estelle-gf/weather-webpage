30ff5b7850bc1ffffb45493f3b9de88f52493651
estelle-gf <estellegfont@gmail.com>
2025-05-23 17:37:46 +0100

    added js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+function changeWeather(response) {}
+function searchCity(city) {
+  let apiKey = "2a99380b94355b9foa25076te09bd049";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric"`;
+  axios.get(apiUrl).then(changeWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
+console.log(searchFormElement);
 searchFormElement.addEventListener("submit", handleSearchSubmit);
