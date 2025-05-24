0f972aa7c6262c723260c80e1b004953118d3ec5
estelle-gf <estellegfont@gmail.com>
2025-05-24 14:10:50 +0100

    added icon, changed CSS on the icon and added the days

--- script.js
+++ script.js
@@ -33,25 +33,39 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function correctDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "2a99380b94355b9foa25076te09bd049";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 function displayForecast(response) {
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  console.log(response.data);
   let forecastHTML = "";
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="weather-forecast-day">
-          <div class="weather-forecast-date">${day}</div>
-          <div class="weather-forecast-icon">⛅</div>
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="weather-forecast-day">
+          <div class="weather-forecast-date">${correctDay(day.time)}</div>
+          <div > <img src="${
+            day.condition.icon_url
+          }" class="weather-forecast-icon"/></div>
           <div class="weather-forecast-temperatures">
-            <div class="weather-forecast-temperature">25°C</div>
-            <div class="weather-forecast-temperature">14°C</div>
+            <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.maximum
+            )}°C</div>
+            <div class="weather-forecast-temperature">${Math.round(
+              day.temperature.minimum
+            )}°C</div>
           </div>
         </div>`;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHTML;
